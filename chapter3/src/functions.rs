e38701e6e39015d1b12600813007610c457cd411
Andrew Yatsko <ayatsko@live.com>
2023-10-29 14:28:59 -0400

    Finish 3.3

---
+++ chapter3/src/functions.rs
@@ -0,0 +1,27 @@
+pub fn main(){
+    another_function();
+    function_2(5);
+    print_labeled_measurement(5, 'm');
+    println!("The value of x is: {}", five());
+    println!("The value of x is: {}", add_one(5));
+}
+
+fn another_function() {
+    println!("Another function")
+}
+
+fn function_2(x: i32){
+    println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char){
+    println!("The measurement is {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,5 +1,7 @@
 mod data_types;
+mod functions;
 
 fn main() {
-    data_types::main();
+    // data_types::main();
+    functions::main();
 }
