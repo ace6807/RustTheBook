7f2143d2eb8e3ca9080ac5aaf6649394deb0da19
Andrew Yatsko <ayatsko@live.com>
2023-11-10 09:53:27 -0500

    Use restaurant-v2 lib from bin

--- chapter7/restaurant_v2/src/lib.rs
+++ chapter7/restaurant_v2/src/lib.rs
@@ -3,5 +3,6 @@ mod front_of_house;
 pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
+    println!("Eating!");
     hosting::add_to_waitlist();
 }
--- chapter7/src/main.rs
+++ chapter7/src/main.rs
@@ -1,5 +1,6 @@
-
+use restaurant_v2;
 
 fn main() {
     println!("Hello, world!");
+    restaurant_v2::eat_at_restaurant();
 }
